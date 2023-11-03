f2c2fd108df8672511f7cb0f2ce242028537e7eb
savkk <savkk@bk.ru>
2023-11-03 19:52:33 +0300

    add product class

---
+++ src/main/java/ru/yandex/practicum/Product.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum;
+
+// Запись
+//
+// https://practicum.yandex.ru/webinar-form/qa-automation-engineer-java/
+
+public class Product {
+    private static final int MIN_PRICE = 100;
+    private final int price;
+    private final int discount;
+
+    public Product(int price, int discount) {
+        this.price = price;
+        this.discount = discount;
+    }
+
+    public int getFinalPrice() {
+        if (discount <= 0 || discount > 50) {
+            return price;
+        }
+        return price - price * discount / 100;
+    }
+
+    public int getPrice() {
+        if (price < MIN_PRICE) {
+            return MIN_PRICE;
+        }
+        return price;
+    }
+}
